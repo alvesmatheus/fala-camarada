593a6f99124b7abd2ac817deb65edfbe45083643
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2021-02-11 17:45:47 -0300

    Adiciona estrutura do módulo de coletores (Scrapy)

---
+++ collectors/settings.py
@@ -0,0 +1,12 @@
+BOT_NAME = 'coletor-fala-camarada'
+
+SPIDER_MODULES = ['collectors.spiders']
+NEWSPIDER_MODULE = 'collectors.spiders'
+
+USER_AGENT = 'Fala Camarada (github.com/alvesmatheus/fala-camarada)'
+
+COOKIES_ENABLED = False
+
+ROBOTSTXT_OBEY = True
+
+AUTOTHROTTLE_ENABLED = True
